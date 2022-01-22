f13810aed9c85e062fecddeb87ac3b4f9acd8a9e
stranothus <stranothus@gmail.com>
2022-01-22 13:41:10 -0600

    Functions -> classes and 2 space tabs -> 4 spaces

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom";
+import ReactDOM, { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
@@ -6,21 +6,24 @@ import About from "./pages/About";
 import News from "./pages/News";
 import Staff from "./pages/Staff";
 import FourOFour from "./pages/FourOFour";
+import React from "react";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="news" element={<News />} />
-          <Route path="staff" element={<Staff />} />
-          <Route path="*" element={<FourOFour />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+export default class App extends React.Component {
+    render() {
+        return (
+            <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="news" element={<News />} />
+                <Route path="staff" element={<Staff />} />
+                <Route path="*" element={<FourOFour />} />
+                </Route>
+            </Routes>
+            </BrowserRouter>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,28 +1,31 @@
+import React from "react";
 import { Outlet, Link } from "react-router-dom";
 
-const Layout = () => {
-  return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/news">News</Link>
-          </li>
-          <li>
-            <Link to="/staff">Staff</Link>
-          </li>
-        </ul>
-      </nav>
+class Layout extends React.Component {
+    render() {
+        return (
+            <>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/news">News</Link>
+                        </li>
+                        <li>
+                            <Link to="/staff">Staff</Link>
+                        </li>
+                    </ul>
+                </nav>
 
-      <Outlet />
-    </>
-  )
+                <Outlet />
+            </>
+        )
+    }
 };
 
 export default Layout;
