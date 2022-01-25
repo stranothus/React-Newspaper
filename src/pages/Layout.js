b472611925efe04663717128ffaa31cd23d8ff4d
stranothus <stranothus@gmail.com>
2022-01-25 08:47:07 -0600

    Update packages and remove unused vars

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import NavBar from "../utils/NavBar/index";
 
 class Layout extends React.Component {
