35795be5254fd468bed866c72f8b2175e30f4308
stranothus <stranothus@gmail.com>
2022-02-07 14:00:56 -0600

    Minor navbar and image header styling

--- src/pages/Staff/index.js
+++ src/pages/Staff/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import LowerSquiggle from "../../utils/LowerSquiggle/index";
+import ImageHeader from "../../utils/ImageHeader/index";
 import StaffList from "../../utils/StaffList/index";
 import "./style.scss";
 
@@ -7,7 +8,7 @@ class Staff extends React.Component {
     render() {
         return (
             <>
-                <h1>Staff</h1>
+                <ImageHeader src = "https://cached.imagescaler.hbpl.co.uk/resize/scaleHeight/815/cached.offlinehbpl.hbpl.co.uk/news/OMC/staff-20161206094148580.jpg">Staff</ImageHeader>
                 <LowerSquiggle>
                     <h2>About our staff</h2>
                     <p>Where would our newspaper be without our wonderful staff? Each staff member is critical to the success of our team as a whole. Here at KA News, we value each and every dedicated member of our team. Go ahead and meet them!</p>
